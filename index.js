32ac4fa9dc86f884e790e734bb7d2d0258df2098
Rishab Singh <54115599+Rksingh090@users.noreply.github.com>
2025-01-14 18:26:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
     })
 
 app.use(cors({
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000", "http://localhost:42761", "http://0.0.0.0:42761"]
 }))
 
 app.use(express.json())
